db46807ae476adfb316d80a77d219fc7e259ae2c
Ebrahim1133 <em034578@gmail.com>
2022-05-11 01:05:46 +0200

    todo

---
+++ lib/ui/pages/add_task_page.dart
@@ -0,0 +1,350 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+import '../../controllers/task_controller.dart';
+import '../../models/task.dart';
+import '../theme.dart';
+import '../widgets/button.dart';
+import '../widgets/input_field.dart';
+
+class AddTaskPage extends StatefulWidget {
+  const AddTaskPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  final TaskController _taskController = Get.put(TaskController());
+  final TextEditingController _tittleEditingController =
+      TextEditingController();
+  final TextEditingController _noteEditingController = TextEditingController();
+  late DateTime _selectedDate = DateTime.now();
+  String _startTime = DateFormat('hh:mm a').format(DateTime.now()).toString();
+  String _endTime = DateFormat('hh:mm a')
+      .format(DateTime.now().add(const Duration(minutes: 15)))
+      .toString();
+  int _selectedRemind = 5;
+  List<int> remindList = [5, 10, 15, 20];
+
+  String _selectedRepeat = 'None';
+  List<String> repeatList = ['None', 'Daily', 'Weekly', 'Monthly'];
+  int _selectedColor = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
+      appBar: appbar(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Title',
+                style: Themes().titleStyle,
+              ),
+              const SizedBox(height: 10),
+              inputField(
+                  hintText: 'Title', controller: _tittleEditingController),
+              const SizedBox(height: 10),
+              Text(
+                'Note',
+                style: Themes().titleStyle,
+              ),
+              const SizedBox(height: 10),
+              inputField(hintText: 'Note', controller: _noteEditingController),
+              const SizedBox(height: 10),
+              Text(
+                'Date',
+                style: Themes().titleStyle,
+              ),
+              const SizedBox(height: 10),
+              inputField(
+                  readOnly: true,
+                  hintText:
+                      DateFormat('dd-MM-yyyy').format(_selectedDate).toString(),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () => getDate(),
+                  )),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Start Time',
+                          style: Themes().titleStyle,
+                        ),
+                        const SizedBox(height: 10),
+                        inputField(
+                            readOnly: true,
+                            hintText: _startTime,
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.timer),
+                              onPressed: () => getTime(isStart: true),
+                            )),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'End Time',
+                          style: Themes().titleStyle,
+                        ),
+                        const SizedBox(height: 10),
+                        inputField(
+                            readOnly: true,
+                            hintText: _endTime,
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.timer),
+                              onPressed: () => getTime(isStart: false),
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Remind',
+                style: Themes().titleStyle,
+              ),
+              const SizedBox(height: 10),
+              inputField(
+                readOnly: true,
+                hintText: '$_selectedRemind Remind Early',
+                suffixIcon: DropdownButton(
+                  underline: Container(
+                    height: 0,
+                  ),
+                  icon: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.keyboard_arrow_down,
+                      size: 34,
+                    ),
+                  ),
+                  items: remindList
+                      .map<DropdownMenuItem<String>>(
+                        (value) => DropdownMenuItem(
+                          value: value.toString(),
+                          child: Text('$value'),
+                        ),
+                      )
+                      .toList(),
+                  style: Themes().subTitleStyle,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedRemind = int.parse(value!);
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Repeat',
+                style: Themes().titleStyle,
+              ),
+              const SizedBox(height: 10),
+              inputField(
+                readOnly: true,
+                hintText: _selectedRepeat,
+                suffixIcon: DropdownButton(
+                  underline: Container(
+                    height: 0,
+                  ),
+                  icon: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.keyboard_arrow_down,
+                      size: 34,
+                    ),
+                  ),
+                  items: repeatList
+                      .map<DropdownMenuItem<String>>(
+                        (value) => DropdownMenuItem(
+                          value: value.toString(),
+                          child: Text(value),
+                        ),
+                      )
+                      .toList(),
+                  style: Themes().subTitleStyle,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedRepeat = value!;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  colorpalette(),
+                  MyButton(
+                      label: 'Creat Task',
+                      ontap: () {
+                        valdidateDate();
+                      })
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar appbar() => AppBar(
+          actions: const [
+            CircleAvatar(
+              backgroundImage: AssetImage('images/person.jpeg'),
+            ),
+            SizedBox(
+              width: 20,
+            )
+          ],
+          elevation: 0,
+          backgroundColor: context.theme.backgroundColor,
+          leading: IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              color: primaryClr,
+            ),
+          ));
+  addTasktoDB() async {
+    int value = await _taskController.addTasks(
+        task: Task(
+            title: _tittleEditingController.text,
+            note: _noteEditingController.text,
+            isCompleted: 0,
+            date: DateFormat.yMd().format(_selectedDate),
+            startTime: _startTime,
+            endTime: _endTime,
+            color: _selectedColor,
+            remind: _selectedRemind,
+            repeat: _selectedRepeat));
+
+    print(value);
+  }
+
+  valdidateDate() {
+    if (_tittleEditingController.text.isNotEmpty &&
+        _noteEditingController.text.isNotEmpty) {
+      addTasktoDB();
+      Get.back();
+    } else if (_tittleEditingController.text.isEmpty ||
+        _noteEditingController.text.isEmpty) {
+      Get.snackbar(
+        'required',
+        'All Fields are required !',
+        margin: const EdgeInsets.all(20),
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.white,
+        colorText: pinkClr,
+        icon: const Icon(
+          Icons.warning_amber_rounded,
+          color: Colors.red,
+        ),
+        isDismissible: true,
+      );
+    } else {
+      print('############ WARMING ############');
+    }
+  }
+
+  Widget colorpalette() => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Color',
+            style: Themes().titleStyle,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Wrap(
+              children: List.generate(
+                  3,
+                  (int? index) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedColor = index!;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: CircleAvatar(
+                            child: _selectedColor == index
+                                ? const Icon(
+                                    Icons.check,
+                                    color: Colors.white,
+                                    size: 16,
+                                  )
+                                : null,
+                            backgroundColor: (index == 0)
+                                ? primaryClr
+                                : index == 1
+                                    ? pinkClr
+                                    : orangeClr,
+                            radius: 15,
+                          ),
+                        ),
+                      )))
+        ],
+      );
+
+  getTime({required bool isStart}) async {
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: isStart
+          ? TimeOfDay.fromDateTime(DateTime.now())
+          : TimeOfDay.fromDateTime(
+              DateTime.now().add(const Duration(minutes: 15))),
+    );
+    String formatedtime = pickedTime!.format(context);
+
+    if (isStart) {
+      setState(() {
+        _startTime = formatedtime;
+      });
+    } else if (!isStart) {
+      setState(() {
+        _endTime = formatedtime;
+      });
+    } else {
+      print('time canceld or something wrong');
+    }
+  }
+
+  getDate() async {
+    DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate,
+        firstDate: DateTime(2022),
+        lastDate: DateTime(2050));
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    } else {
+      print('its null or something wrong');
+    }
+  }
+}
---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,342 @@
+// ignore_for_file: unused_field, prefer_const_constructors, unused_element, unrelated_type_equality_checks
+
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+import '../../controllers/task_controller.dart';
+import '../../models/task.dart';
+import '../../services/notification_services.dart';
+import '../../services/theme_services.dart';
+import '../size_config.dart';
+import '../theme.dart';
+import '../widgets/button.dart';
+import '../widgets/task_tile.dart';
+import 'add_task_page.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late NotifyHelper notifyHelper;
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.requestIOSPermissions();
+    notifyHelper.initializeNotification();
+
+    _taskController.getTasks();
+  }
+
+  final TaskController _taskController = Get.put(TaskController());
+  DateTime selectedDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+        backgroundColor: context.theme.backgroundColor,
+        appBar: appbar(),
+        body: Column(
+          children: [
+            _addtaskBar(),
+            _addDateBar(),
+            SizedBox(
+              height: 20,
+            ),
+            _showTasks(),
+          ],
+        ));
+  }
+
+  AppBar appbar() => AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.cleaning_services,
+                size: 24,
+                color: Get.isDarkMode ? Colors.white : darkGreyClr,
+              ),
+              onPressed: () {
+                notifyHelper.cancelAllNotification();
+                _taskController.deleteAllTasks();
+              },
+            ),
+            CircleAvatar(
+              backgroundImage: AssetImage('images/person.jpeg'),
+            ),
+            SizedBox(
+              width: 20,
+            )
+          ],
+          elevation: 0,
+          backgroundColor: context.theme.backgroundColor,
+          leading: IconButton(
+            icon: Icon(
+              Get.isDarkMode ? Icons.wb_sunny : Icons.nightlight_round_rounded,
+              size: 24,
+              color: Get.isDarkMode ? Colors.white : darkGreyClr,
+            ),
+            onPressed: () {
+              ThemeServices().switchTheme();
+              /* NotifyHelper().dispalyNotification(
+                  title: 'theme changed', body: 'theme changed');*/
+              //  NotifyHelper().scheduledNotifications();
+            },
+          ));
+
+  _addtaskBar() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat.yMMMMd().format(DateTime.now()).toString(),
+                style: Themes().headingStyle,
+              ),
+              Text(
+                'Today',
+                style: Themes().headingStyle,
+              ),
+            ],
+          ),
+          MyButton(
+            label: '+ Add Task',
+            ontap: () async {
+              await Get.to(() => const AddTaskPage());
+              _taskController.getTasks();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  _addDateBar() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: DatePicker(
+        DateTime.now(),
+        width: 80,
+        height: 100,
+        selectedTextColor: Colors.white,
+        selectionColor: primaryClr,
+        onDateChange: (newDate) {
+          setState(() {
+            selectedDate = newDate;
+          });
+        },
+        dateTextStyle: GoogleFonts.lato(
+            textStyle: const TextStyle(
+                color: Colors.grey, fontSize: 20, fontWeight: FontWeight.w600)),
+        monthTextStyle: GoogleFonts.lato(
+            textStyle: TextStyle(
+                color: Colors.grey, fontSize: 16, fontWeight: FontWeight.w600)),
+        dayTextStyle: GoogleFonts.lato(
+            textStyle: TextStyle(
+                color: Colors.grey, fontSize: 12, fontWeight: FontWeight.w600)),
+        initialSelectedDate: selectedDate,
+      ),
+    );
+  }
+
+  _showTasks() {
+    return Expanded(
+      child: Obx(() {
+        if (_taskController.taskList.isEmpty) {
+          return _notTasks();
+        } else
+          return ListView.builder(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: SizeConfig.orientation == Orientation.landscape
+                ? Axis.horizontal
+                : Axis.vertical,
+            itemCount: _taskController.taskList.length,
+            itemBuilder: (context, index) {
+              var task = _taskController.taskList[index];
+
+              if (task.repeat == 'Daily' ||
+                  task.date == DateFormat().add_yMd().format(selectedDate) ||
+                  (task.repeat == 'Weekly' &&
+                      selectedDate
+                                  .difference(
+                                      DateFormat().add_yMd().parse(task.date!))
+                                  .inDays %
+                              7 ==
+                          0) ||
+                  (task.repeat == 'Monthly' &&
+                      DateFormat().parse(task.date!).day == selectedDate)) {
+                var hour = task.startTime.toString().split(':')[0];
+                var minutes = task.startTime.toString().split(':')[1];
+                notifyHelper.scheduledNotification(
+                    int.parse(hour), int.parse(minutes.split('')[0]), task);
+                return AnimationConfiguration.staggeredList(
+                  duration: Duration(milliseconds: 800),
+                  position: index,
+                  child: SlideAnimation(
+                    horizontalOffset: 300,
+                    child: FadeInAnimation(
+                      child: GestureDetector(
+                        onTap: () => showBottomSheet(context, task),
+                        child: TaskTile(task),
+                      ),
+                    ),
+                  ),
+                );
+              } else {
+                return Container();
+              }
+            },
+          );
+      }),
+    );
+  }
+
+  _notTasks() {
+    return Stack(
+      children: [
+        AnimatedPositioned(
+          duration: Duration(microseconds: 2000),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              direction: SizeConfig.orientation == Orientation.landscape
+                  ? Axis.horizontal
+                  : Axis.vertical,
+              children: [
+                SizeConfig.orientation == Orientation.landscape
+                    ? SizedBox(height: 6)
+                    : SizedBox(height: 100),
+                SvgPicture.asset(
+                  'images/task.svg',
+                  height: 90,
+                  color: primaryClr.withOpacity(0.5),
+                ),
+                Text(
+                  'You do not have any task yet!\n add new task to make your days productive',
+                  style: Themes().subTitleStyle,
+                  textAlign: TextAlign.center,
+                ),
+                SizeConfig.orientation == Orientation.landscape
+                    ? SizedBox(height: 120)
+                    : SizedBox(height: 180),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  _buildBottomSheet(
+      {required String label,
+      required Function() onTap,
+      required Color clr,
+      bool isClose = false}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 3),
+        height: 65,
+        width: SizeConfig.screenWidth * 0.9,
+        decoration: BoxDecoration(
+          border: Border.all(
+              width: 2,
+              color: isClose
+                  ? Get.isDarkMode
+                      ? Colors.grey[600]!
+                      : Colors.grey[300]!
+                  : clr),
+          borderRadius: BorderRadius.circular(20),
+          color: isClose ? Colors.transparent : clr,
+        ),
+        child: Center(
+          child: Text(
+            label,
+            style: isClose
+                ? Themes().titleStyle
+                : Themes().titleStyle.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  showBottomSheet(BuildContext context, Task task) {
+    Get.bottomSheet(
+      SingleChildScrollView(
+        child: Container(
+            padding: EdgeInsets.only(top: 4),
+            width: SizeConfig.screenWidth,
+            height: (SizeConfig.orientation == Orientation.landscape)
+                ? (task.isCompleted == 1
+                    ? SizeConfig.screenHeight * 0.6
+                    : SizeConfig.screenHeight * 0.8)
+                : (task.isCompleted == 1
+                    ? SizeConfig.screenHeight * 0.30
+                    : SizeConfig.screenHeight * 0.39),
+            color: Get.isDarkMode ? darkHeaderClr : Colors.white,
+            child: Center(
+              child: Column(
+                children: [
+                  Flexible(
+                      child: Container(
+                    height: 6,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Get.isDarkMode
+                            ? Colors.grey[600]
+                            : Colors.grey[300]),
+                  )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  task.isCompleted == 1
+                      ? Container()
+                      : _buildBottomSheet(
+                          label: 'Task completed',
+                          onTap: () {
+                            _taskController.markuscompleted(task.id!);
+                            Get.back();
+                          },
+                          clr: primaryClr),
+                  _buildBottomSheet(
+                      label: 'Delete ',
+                      onTap: () {
+                        notifyHelper.cancelNotification(task);
+                        _taskController.deleteTasks(task);
+                        Get.back();
+                      },
+                      clr: primaryClr),
+                  _buildBottomSheet(
+                      label: 'Cancel ',
+                      onTap: () {
+                        Get.back();
+                      },
+                      clr: primaryClr),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
